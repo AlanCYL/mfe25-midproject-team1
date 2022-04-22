3c075a9bd2921f49bf7f4c50bfc57b89ae531afa
remi06vera <verayyone@gmail.com>
2022-04-22 13:32:02 +0800

    Update db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -1,18 +1,19 @@
 <?php
+//與資料庫連結
+
 $servername = "localhost";
-$username = "admin";
-$password = "12345";
-$dbname = "mf251";
+$username = "mf251";
+$password = "2525";
+$dbname = "project";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // 檢查連線
 if ($conn->connect_error) {
-    die("連線失敗: " . $conn->connect_error);
+  	die("連線失敗: " . $conn->connect_error);
 }else{
-    // echo "連線成功";
+    //echo "連線成功";
 }
-
 session_start();
 
 ?>
