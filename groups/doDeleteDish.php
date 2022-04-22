054f793d3ff46c1bda01243ff6e6c2bace14f2f9
jayna922 <leeyijie922@gmail.com>
2022-04-22 16:56:03 -0700

    dish
    dish 刪除跳頁

--- groups/doDeleteDish.php
+++ groups/doDeleteDish.php
@@ -1,13 +1,13 @@
 <?php
 require_once("../db-connect.php");
-
+$shopID=1;
 $dish_id=$_GET["card"];
 
 $sql="DELETE FROM dish WHERE dish_id='$dish_id'";
 
 
 if ($conn->query($sql) === TRUE) {
-    $url = "dish-list.php?shop=<?=$shopID?>";
+    $url = "dish-list.php?shop=$shopID";
     header("location: ".$url);
 } else {
     echo "刪除資料錯誤: " . $conn->error;
