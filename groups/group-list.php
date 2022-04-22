a7fbf7d8fdf55150c1db653b6a4977e8793121e8
jayna922 <leeyijie922@gmail.com>
2022-04-22 15:28:30 -0700

    Update group-list.php

--- groups/group-list.php
+++ groups/group-list.php
@@ -176,7 +176,7 @@ $rows=$result->fetch_all(MYSQLI_ASSOC);
 
         <div class="d-flex justify-content-between mb-4">
           <div>
-            <h2 class="mb-4"><?=$row["shop_name"]?> 商家所有開團清單</h2>
+            <h2 class="mb-4">商家所有開團清單</h2>
             <!-- search -->
             <div class="input-group">
               <div class="form-outline">
