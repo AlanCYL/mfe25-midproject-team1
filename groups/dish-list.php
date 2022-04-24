7234eeea2d6613e070baf77e8d3d217380405ff8
jayna <leeyijie922@gmail.com>
2022-04-24 00:12:07 -0700

    same

--- groups/dish-list.php
+++ groups/dish-list.php
@@ -126,7 +126,7 @@ $shop_id=$_GET["shop"];
 
       </div>
       <div class="" style="margin-top:100px;margin-left: 800px;margin-bottom: 30px">
-        <a class="btn btn-secondary" style="width:100px;" href="dish.php?dish=<?=$shopID?>">返回上架</a>
+        <a class="btn btn-secondary" style="width:100px;" href="dish.php?login=<?=$shopID?>&dish=<?=$shopID?>">返回上架</a>
       </div>
 
 
