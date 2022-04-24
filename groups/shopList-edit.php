45f4353b45617fc9ce210aefaa3362b452aff9d3
leeyijie <leeyijie922@gmail.com>
2022-04-24 14:36:23 -0700

    shopList

--- groups/shopList-edit.php
+++ groups/shopList-edit.php
@@ -13,6 +13,7 @@ shop.service_id = shop_service.service_id AND
 shop.shop_id=$shopID
 ";
 
+
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 // if(!isset($row)){
@@ -34,7 +35,7 @@ $rows2=$result2->fetch_all(MYSQLI_ASSOC);
 <html lang="en">
 
 <head>
-    <title>店家明細</title>
+    <title>店家明細修改</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -69,13 +70,13 @@ $rows2=$result2->fetch_all(MYSQLI_ASSOC);
             height: 256px;
             border: 1px solid black;
             /* background-image: url(./logo/kangyaolife.jpg); */
+
         }
 
         .object-cover {
             width: 100%;
             height: 100%;
             object-fit: cover;
-            background-image
         }
 
         .aroundimg {
@@ -109,24 +110,23 @@ $rows2=$result2->fetch_all(MYSQLI_ASSOC);
                     </button>
                     <div class="collapse show" id="home-collapse">
                         <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="shopList.php?login=<?=$shopID?>" class="link-dark rounded">店家清單</a></li>
+                            <li><a href="shop_list.php" class="link-dark rounded">店家清單</a></li>
+                            <li><a href="shop_groups_list.php" class="link-dark rounded">開團清單</a></li>
                         </ul>
                     </div>
                 </li>
                 <li class="mb-1">
-          <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-            data-bs-target="#dashboard-collapse" aria-expanded="false">
-            開團管理
-          </button>
-          <div class="collapse" id="dashboard-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-              <li><a href="group-list.php?login=<?=$shopID?>" class="link-dark rounded">開團清單</a></li>
-              <li><a href="group-open.php?login=<?=$shopID?>&open=<?=$shopID?>" class="link-dark rounded">上架開團</a></li>
-              <li><a href="dish-list.php?login=<?=$shopID?>&shop=<?=$shopID?>" class="link-dark rounded">菜式清單</a></li>
-              <li><a href="dish.php?login=<?=$shopID?>&dish=<?=$shopID?>" class="link-dark rounded">上架菜式</a></li>
-            </ul>
-          </div>
-        </li>
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#dashboard-collapse" aria-expanded="false">
+                        會員管理
+                    </button>
+                    <div class="collapse" id="dashboard-collapse">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="#" class="link-dark rounded">會員清單</a></li>
+                            <li><a href="#" class="link-dark rounded">優惠制度</a></li>
+                        </ul>
+                    </div>
+                </li>
                 <li class="mb-1">
                     <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
                         data-bs-target="#orders-collapse" aria-expanded="false">
@@ -138,6 +138,7 @@ $rows2=$result2->fetch_all(MYSQLI_ASSOC);
                             <li><a href="#" class="link-dark rounded">會員意見反應</a></li>
 
                         </ul>
+                    </div>
                 </li>
             </ul>
         </nav>
@@ -156,84 +157,92 @@ $rows2=$result2->fetch_all(MYSQLI_ASSOC);
             </div>
             <div>
                 <!-- 可以放content -->
+                <form action="shopList-updateshop.php" method="POST">
 
                 <div class="container">
-                      <div class="row">
-                          <div class="col-4">
-                              <div class="logo">
-                                <img class="object-cover" src="image/<?=$row["img"];?>"  alt="">
-                              </div>
-                          </div>
-                          <div class="col-6 d-flex align-items-center">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="logo">
+                            <img class="object-cover" src="image/<?=$row["img"];?>"  alt="">
+                            </div>
+                        </div>
+
+                        <div class="col-6 d-flex align-items-center">
                             <table class="table table-bordered">
                                 <tr>
                                     <th>商家編號</th>
-                                    <td><?=$row["shop_id"];?></td>
+                                    <input type="hidden" name="shop_id" value="<?=$row["shop_id"]?>">
+                                    <td><input type="id" name="shop_id" class="form-control" value="<?=$row["shop_id"];?>" disabled></td>
                                 </tr>
                                 <tr>
                                     <th>商家名稱</th>
-                                    <td><?=$row["shop_name"];?></td>
+                                    <td><input type="text" name="shop_name" class="form-control" value="<?=$row["shop_name"];?>"></td>
                                 </tr>
                                 <tr>
-                                    <th>email</th>
-                                    <td><?=$row["shop_account"];?></td>
+                                    <th>商家帳號</th>
+                                    <td><input type="text" name="shop_account" class="form-control" value="<?=$row["shop_account"];?>"></td>
                                 </tr>
                                 <tr>
-                                    <th>phone</th>
-                                    <td><?=$row["shop_phone"];?></td>
+                                    <th>商家電話</th>
+                                    <td><input type="tel" name="shop_phone" class="form-control" value="<?=$row["shop_phone"];?>"></td>
                                 </tr>
                                 <tr>
-                                    <th>地址</th>
-                                    <td><?=$row["shop_address"];?></td>
+                                    <th>商家地址</th>
+                                    <td><input type="text" name="shop_address" class="form-control" value="<?=$row["shop_address"];?>"></td>
                                 </tr>
                             </table>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="container pt-5">
-                        <table class="table table-bordered">
-                            <tr>
-                                <th>店家簡介</th>
-                                <td><?=$row["shop_description"];?>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>店家服務</th>
-                                <td>
-                                    <li><?=$row["service_name"];?></li>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>餐廳類別</th>
-                                <td>
-
-                                    <li><?=$row["type_name"];?></li>
-
-                                </td>
-                            </tr>
-                        </table>
-                  </div>
-                  <!-- <div class="container pt-5">
-                      <h3>店家環境圖</h3>
-                      <div class="d-flex flex-row ">
-                          <div class="aroundimg">
-                              <img class="object-cover" src="images\NINI_2.jpg" alt="">
-                          </div>
-                          <div class="aroundimg ps-4">
-                            <img class="object-cover" src="images\NINI_4.jpg" alt="">
-                          </div>
-                          <div class="aroundimg ps-4">
-                            <img class="object-cover" src="images\NINI_5.jpg" alt="">
-                          </div>
-                      </div> -->
-                      <!-- <div class="pt-3  d-flex justify-content-end">
-                        <a class="" href="" class="btn btn-info ">編輯</a>
-                        <a href="" class="btn btn-danger">刪除</a>
-                      </div> -->
-                      <div class="py-2 d-flex justify-content-end">
-                      <a href="shopList-edit.php?login=<?=$row["shop_id"];?>" class="btn btn-info text-white ms-2">編輯</a>
+                        </div>
                     </div>
-                  </div>
+                </div>
+                <div class="container pt-5">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>店家簡介</th>
+                            <td><input type="text" name="shop_description" class="form-control shop_description" value="<?=$row["shop_description"];?>" maxlength="100">
+                            </td>
+                        </tr>
+                        <tr>
+                        <th>店家服務</th>
+                            <td>
+                                <select name="service_id" id="" place>
+
+                                    <option value="1">免服務費</option>
+                                    <option value="2">服務費10%</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>菜式種類</th>
+                            <td>
+                            <li><?=$row["type_name"];?></li>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <!-- <div class="container pt-5">
+                    <h3>店家環境圖</h3>
+                    <div class="mb-2">
+                        <input type="file" class="form-control" name="myFile" accept=".jpg, .jpeg, .png, .svg">
+                    </div>
+                    <button class="btn btn-info mb-2" type="submit">送出</button>
+                    <div class="d-flex flex-row ">
+                        <div class="arounding">
+                            <img class="object-cover" src="images\NINI_2.jpg" alt="">
+                        </div>
+                        <div class="arounding ps-4">
+                            <img class="object-cover" src="images\NINI_4.jpg" alt="">
+                        </div>
+                        <div class="arounding ps-4">
+                            <img class="object-cover" src="images\NINI_5.jpg" alt="">
+                        </div>
+                    </div> -->
+                    <div class="pt-5 d-flex justify-content-end">
+                        <button type="submit" class="btn btn-info me-2">儲存</button>
+                        <a href="shopList.php?login=<?=$row["shop_id"]?>" class="btn btn-info btn-danger">取消</a>
+                    </div>
+                </div>
+            </div>
+            </form>
 
 
 
@@ -249,7 +258,7 @@ $rows2=$result2->fetch_all(MYSQLI_ASSOC);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
         crossorigin="anonymous"></script>
-        <script src="../template/sidebars.js"></script>
+        <link rel="stylesheet" href="/mid-project/template/sidebars.js">
 </body>
 
 </html>
