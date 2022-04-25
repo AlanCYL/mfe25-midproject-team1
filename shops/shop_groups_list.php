62361a31aea8a18979e5ff949cbfcb2e101834f2
remi06vera <verayyone@gmail.com>
2022-04-25 11:42:33 +0800

    title

--- shops/shop_groups_list.php
+++ shops/shop_groups_list.php
@@ -73,7 +73,7 @@ $groups_count=$result->num_rows; //groups資料庫共幾筆資料
 <html lang="en">
 
 <head>
-    <title>開團清單</title>
+    <title>開團清單總覽</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
